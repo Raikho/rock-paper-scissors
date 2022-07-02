b6f519ab8079921a7ac842bf7d7aca42c855bc64
Mateen <zahidx297@yahoo.com>
2022-07-02 13:15:43 -0400

    get computer's selection

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+// TODO: create function for player selection
+
+// create function for computer selection
+function computerPlay() {
+    let options = ["Rock", "Paper", "Scissors"];
+    let computerSelection = options[Math.floor(Math.random() * 3)];
+    console.log(`Computer selection: ${computerSelection}`);
+}
+// TODO: create function to calculate winner
+
+// TODO: run game
+computerPlay();
